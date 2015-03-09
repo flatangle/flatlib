6fcba206724eba1493a442e811be2a9e60ad3965
João Ventura <joaojonesventura@gmail.com>
2015-03-09 20:32:05 +0000

    Add paths to flatlib

--- flatlib/__init__.py
+++ flatlib/__init__.py
@@ -3,3 +3,10 @@
     Author: João Ventura (flatangleweb@gmail.com)
 
 """
+
+import os
+
+
+# Library and resource paths
+PATH_LIB = os.path.dirname(__file__) + os.sep
+PATH_RES = PATH_LIB + 'resources' + os.sep
