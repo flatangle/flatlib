46f2ce07deb066e8f9e95a10623e564c1ecf33ab
João Ventura <joaojonesventura@gmail.com>
2015-03-13 11:58:45 +0000

    Add aspect names to properties

--- flatlib/props.py
+++ flatlib/props.py
@@ -318,7 +318,27 @@ class house:
 # --------------------- #
 
 class aspect:
-    pass
+    
+    # Names
+    name = {
+        # Major Aspects
+        const.NO_ASPECT: 'None',
+        const.CONJUNCTION: 'Conjunction',
+        const.SEXTILE: 'Sextile',
+        const.SQUARE: 'Square',
+        const.TRINE: 'Trine',
+        const.OPPOSITION: 'Opposition',
+        
+        # Minor Aspects
+        const.SEMISEXTILE: 'Semisextile',
+        const.SEMIQUINTILE: 'Semiquintile',
+        const.SEMISQUARE: 'Semisquare',
+        const.QUINTILE: 'Quintile',
+        const.SESQUIQUINTILE: 'Sesquiquintile',
+        const.SESQUISQUARE: 'Sesquisquare',
+        const.BIQUINTILE: 'Biquintile',
+        const.QUINCUNX: 'Quincunx'
+    }
 
 
 # ------------------------- #
