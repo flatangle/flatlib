b5f33d35c7c450cc05f9da2e077c832bde5a49ee
João Ventura <joaojonesventura@gmail.com>
2015-03-18 12:08:25 +0000

    Add essential dignities recipe

---
+++ recipes/essentialdignities.py
@@ -0,0 +1,34 @@
+"""
+    Author: João Ventura <flatangleweb@gmail.com>
+    
+    
+    This recipe shows sample code for handling 
+    essential dignities.
+
+"""
+
+from flatlib import const
+from flatlib.chart import Chart
+from flatlib.datetime import Datetime
+from flatlib.geopos import GeoPos
+from flatlib.dignities import essential
+
+
+# Build a chart for a date and location
+date = Datetime('2015/03/13', '17:00', '+00:00')
+pos = GeoPos('38n32', '8w54')
+chart = Chart(date, pos)
+
+# Get the Asc ruler
+asc = chart.get(const.ASC)
+ascRulerID = essential.ruler(asc.sign)
+ascRuler = chart.get(ascRulerID)
+print(ascRuler)   # <Mercury Pisces +00:48:57 +01:29:49>
+
+# Get the Asc ruler score
+score = essential.score(ascRuler.id, ascRuler.sign, ascRuler.signlon)
+print(score)
+
+# Simples alternative using the EssentialInfo class
+info = essential.EssentialInfo(ascRuler)
+print(info.score)
