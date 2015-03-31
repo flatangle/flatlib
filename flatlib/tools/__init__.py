1a2c85ac226b1a065cf19f2b19525dec4cbe2593
João Ventura <joaojonesventura@gmail.com>
2015-03-31 11:48:35 +0100

    Create tools subpackage

---
+++ flatlib/tools/__init__.py
@@ -0,0 +1,9 @@
+"""
+    This file is part of flatlib - (C) FlatAngle
+    Author: João Ventura (flatangleweb@gmail.com)
+    
+    
+    This subpackage implements some traditional
+    astrology tools. 
+  
+"""
