c2950dc07b8ac52a71d09341c2f72960ecb37729
João Ventura <joaojonesventura@gmail.com>
2015-04-06 10:52:14 +0100

    Add protection recipe

---
+++ recipes/profections.py
@@ -0,0 +1,28 @@
+"""
+    Author: João Ventura <flatangleweb@gmail.com>
+    
+    
+    This recipe shows sample code for handling 
+    profections.
+
+"""
+
+from flatlib import const
+from flatlib.chart import Chart
+from flatlib.datetime import Datetime
+from flatlib.geopos import GeoPos
+from flatlib.predictives import profections
+
+
+# Build a chart for a date and location
+date = Datetime('2011/03/13', '17:00', '+00:00')
+pos = GeoPos('38n32', '8w54')
+chart = Chart(date, pos)
+
+# Get the profection Chart for a date
+today = Datetime('2015/04/06', '10:40', '+01:00')
+pChart = profections.compute(chart, today)
+
+# Print the Asc
+res = pChart.get(const.ASC)
+print(res)  #  <Asc Capricorn +05:23:06>
