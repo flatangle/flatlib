e60d11ca9e3869233efce38bbe6dd437508fc492
João Ventura <joaojonesventura@gmail.com>
2015-04-08 10:05:48 +0100

    Create protocols package

---
+++ flatlib/protocols/__init__.py
@@ -0,0 +1,9 @@
+"""
+    This file is part of flatlib - (C) FlatAngle
+    Author: João Ventura (flatangleweb@gmail.com)
+    
+    
+    This subpackage implements a few traditional 
+    astrology protocol techniques. 
+  
+"""
