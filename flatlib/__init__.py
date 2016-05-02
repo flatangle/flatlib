c441f4da5ac1f73b291b32d22ada7a3a1bff01fa
João Ventura <joaojonesventura@gmail.com>
2016-05-02 17:02:37 +0100

    Bump version to 0.2.1

--- flatlib/__init__.py
+++ flatlib/__init__.py
@@ -7,7 +7,7 @@
 import os
 
 
-__version__ = '0.2.0' 
+__version__ = '0.2.1'
 
 # Library and resource paths
 PATH_LIB = os.path.dirname(__file__) + os.sep
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 setup(
     # Project
     name = 'flatlib',
-    version = '0.2.0',
+    version = '0.2.1',
     
     # Sources
     packages = find_packages(),
