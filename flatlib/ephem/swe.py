fc4cf6a7657d3face081f24965429c84ef1c1a64
Stanislas Marquis <stan@astrorigin.com>
2020-04-06 05:48:51 +0200

    Updates required by pyswisseph 2.08.00-0
    Fixes #40, #41

--- flatlib/ephem/swe.py
+++ flatlib/ephem/swe.py
@@ -1,14 +1,14 @@
 """
     This file is part of flatlib - (C) FlatAngle
     Author: João Ventura (flatangleweb@gmail.com)
-    
-    
-    This module implements a simple interface with the C 
+
+
+    This module implements a simple interface with the C
     Swiss Ephemeris using the pyswisseph library.
-    
+
     The pyswisseph library must be already installed and
     accessible.
-  
+
 """
 
 import swisseph
@@ -20,22 +20,22 @@ from flatlib import const
 SWE_OBJECTS = {
     const.SUN: 0,
     const.MOON: 1,
-    const.MERCURY: 2, 
+    const.MERCURY: 2,
     const.VENUS: 3,
     const.MARS: 4,
-    const.JUPITER: 5, 
+    const.JUPITER: 5,
     const.SATURN: 6,
     const.URANUS: 7,
-    const.NEPTUNE: 8, 
+    const.NEPTUNE: 8,
     const.PLUTO: 9,
-    const.CHIRON: 15, 
+    const.CHIRON: 15,
     const.NORTH_NODE: 10
 }
 
 # Map house systems
 SWE_HOUSESYS = {
     const.HOUSES_PLACIDUS: b'P',
-    const.HOUSES_KOCH: b'K', 
+    const.HOUSES_KOCH: b'K',
     const.HOUSES_PORPHYRIUS: b'O',
     const.HOUSES_REGIOMONTANUS: b'R',
     const.HOUSES_CAMPANUS: b'C',
@@ -43,9 +43,9 @@ SWE_HOUSESYS = {
     const.HOUSES_EQUAL_2: b'E',
     const.HOUSES_VEHLOW_EQUAL: b'V',
     const.HOUSES_WHOLE_SIGN: b'W',
-    const.HOUSES_MERIDIAN: b'X', 
+    const.HOUSES_MERIDIAN: b'X',
     const.HOUSES_AZIMUTHAL: b'H',
-    const.HOUSES_POLICH_PAGE: b'T', 
+    const.HOUSES_POLICH_PAGE: b'T',
     const.HOUSES_ALCABITUS: b'B',
     const.HOUSES_MORINUS: b'M'
 }
@@ -63,7 +63,7 @@ def setPath(path):
 def sweObject(obj, jd):
     """ Returns an object from the Ephemeris. """
     sweObj = SWE_OBJECTS[obj]
-    sweList = swisseph.calc_ut(jd, sweObj)
+    sweList, flg = swisseph.calc_ut(jd, sweObj)
     return {
         'id': obj,
         'lon': sweList[0],
@@ -71,17 +71,17 @@ def sweObject(obj, jd):
         'lonspeed': sweList[3],
         'latspeed': sweList[4]
     }
-    
+
 def sweObjectLon(obj, jd):
     """ Returns the longitude of an object. """
     sweObj = SWE_OBJECTS[obj]
-    sweList = swisseph.calc_ut(jd, sweObj)
+    sweList, flg = swisseph.calc_ut(jd, sweObj)
     return sweList[0]
 
 def sweNextTransit(obj, jd, lat, lon, flag):
     """ Returns the julian date of the next transit of
-    an object. The flag should be 'RISE' or 'SET'. 
-    
+    an object. The flag should be 'RISE' or 'SET'.
+
     """
     sweObj = SWE_OBJECTS[obj]
     flag = swisseph.CALC_RISE if flag == 'RISE' else swisseph.CALC_SET
@@ -90,29 +90,29 @@ def sweNextTransit(obj, jd, lat, lon, flag):
 
 
 # === Houses and angles === #
-        
+
 def sweHouses(jd, lat, lon, hsys):
     """ Returns lists of houses and angles. """
     hsys = SWE_HOUSESYS[hsys]
     hlist, ascmc = swisseph.houses(jd, lat, lon, hsys)
     # Add first house to the end of 'hlist' so that we
-    # can compute house sizes with an iterator 
+    # can compute house sizes with an iterator
     hlist += (hlist[0],)
     houses = [
         {
             'id': const.LIST_HOUSES[i],
-            'lon': hlist[i], 
+            'lon': hlist[i],
             'size': angle.distance(hlist[i], hlist[i+1])
         } for i in range(12)
     ]
     angles = [
-        {'id': const.ASC, 'lon': ascmc[0]}, 
+        {'id': const.ASC, 'lon': ascmc[0]},
         {'id': const.MC, 'lon': ascmc[1]},
         {'id': const.DESC, 'lon': angle.norm(ascmc[0] + 180)},
         {'id': const.IC, 'lon': angle.norm(ascmc[1] + 180)}
     ]
     return (houses, angles)
-    
+
 def sweHousesLon(jd, lat, lon, hsys):
     """ Returns lists with house and angle longitudes. """
     hsys = SWE_HOUSESYS[hsys]
@@ -120,7 +120,7 @@ def sweHousesLon(jd, lat, lon, hsys):
     angles = [
         ascmc[0],
         ascmc[1],
-        angle.norm(ascmc[0] + 180), 
+        angle.norm(ascmc[0] + 180),
         angle.norm(ascmc[1] + 180)
     ]
     return (hlist, angles)
@@ -128,16 +128,16 @@ def sweHousesLon(jd, lat, lon, hsys):
 
 # === Fixed stars === #
 
-# Beware: the swisseph.fixstar_mag function is really 
-# slow because it parses the fixstars.cat file every 
+# Beware: the swisseph.fixstar_mag function is really
+# slow because it parses the fixstars.cat file every
 # time..
 
 def sweFixedStar(star, jd):
     """ Returns a fixed star from the Ephemeris. """
-    sweList = swisseph.fixstar_ut(star, jd)
-    mag = swisseph.fixstar_mag(star)
+    sweList, stnam, flg = swisseph.fixstar2_ut(star, jd)
+    mag = swisseph.fixstar2_mag(star)
     return {
-        'id': star, 
+        'id': star,
         'mag': mag,
         'lon': sweList[0],
         'lat': sweList[1]
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Project
     name = 'flatlib',
     version = '0.2.2-dev',
-    
+
     # Sources
     packages = find_packages(),
     package_data = {
@@ -21,20 +21,20 @@ setup(
             'resources/swefiles/*'
         ],
     },
-    
+
     # Dependencies
-    install_requires = ['pyswisseph>=2.00.00-2'],
-    
+    install_requires = ['pyswisseph>=2.08.00-0'],
+
     # Metadata
     description = 'Python library for Traditional Astrology',
     url = 'https://github.com/flatangle/flatlib',
     keywords = ['Astrology', 'Traditional Astrology'],
     license = 'MIT',
-    
+
     # Authoring
     author = 'João Ventura',
     author_email = 'flatangleweb@gmail.com',
-    
+
     # Classifiers
     classifiers = [
         'Programming Language :: Python :: 3',
@@ -45,5 +45,5 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ], 
+    ],
 )
