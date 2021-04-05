3c9b81001a747f0b8f290833ed94acc377f83edf
João Ventura <joaojonesventura@gmail.com>
2021-04-05 09:56:47 +0100

    Update pyswisseph to 2.08.00-1

--- setup.py
+++ setup.py
@@ -7,36 +7,35 @@
 from setuptools import setup
 from setuptools import find_packages
 
-
 setup(
     # Project
-    name = 'flatlib',
-    version = '0.2.2-dev',
-    
+    name='flatlib',
+    version='0.2.3',
+
     # Sources
-    packages = find_packages(),
-    package_data = {
+    packages=find_packages(),
+    package_data={
         'flatlib': [
             'resources/README.md',
             'resources/swefiles/*'
         ],
     },
-    
+
     # Dependencies
-    install_requires = ['pyswisseph==2.00.00-2'],
-    
+    install_requires=['pyswisseph==2.08.00-1'],
+
     # Metadata
-    description = 'Python library for Traditional Astrology',
-    url = 'https://github.com/flatangle/flatlib',
-    keywords = ['Astrology', 'Traditional Astrology'],
-    license = 'MIT',
-    
+    description='Python library for Traditional Astrology',
+    url='https://github.com/flatangle/flatlib',
+    keywords=['Astrology', 'Traditional Astrology'],
+    license='MIT',
+
     # Authoring
-    author = 'João Ventura',
-    author_email = 'flatangleweb@gmail.com',
-    
+    author='João Ventura',
+    author_email='flatangleweb@gmail.com',
+
     # Classifiers
-    classifiers = [
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
@@ -45,5 +44,5 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ], 
+    ],
 )
