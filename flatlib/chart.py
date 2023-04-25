d4c44adba8720c9738170f7d6ca3a661fb9b57bd
ruttierut <48896144+ruttierut@users.noreply.github.com>
2023-04-25 22:34:35 +0530

    Update chart.py

--- flatlib/chart.py
+++ flatlib/chart.py
@@ -44,7 +44,7 @@ class Chart:
         
         """
         # Handle optional arguments
-        hsys = kwargs.get('hsys', const.HOUSES_PLACIDUS)
+        hsys = kwargs.get('hsys', const.HOUSES_DEFAULT)
         IDs = kwargs.get('IDs', const.LIST_OBJECTS)
 
         self.date = date
